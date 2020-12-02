490b6cdde1bad7ccffbcbf98771b527f69bdeb63
John Moose <john@mooserific.org>
2020-12-01 19:39:26 -0500

    Remove unused files, add Tailwind, and improve test coverage

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const greeting = screen.getByText(/Hello!/i);
+  expect(greeting).toBeInTheDocument();
 });
